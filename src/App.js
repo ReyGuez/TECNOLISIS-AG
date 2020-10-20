ca77448cb975a5d12f5c2fdb2afcbd99e1ce4fb2
Alex250298 <ma.chavez2502@gmail.com>
2020-10-20 01:11:46 -0500

    The UI web page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to TECNOLISIS AG.
+          Welcome to TECNOLISIS AG xd.
         </p>
         <a
           className="App-link"
